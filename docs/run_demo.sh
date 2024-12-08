ce302cd679d75d4d6165d779464a25518bf7294a
huangjunjie-cs <junjiehuang.cs@gmail.com>
2024-12-08 16:59:54 +0800

    bugfix: rename filename

--- docs/run_demo.sh
+++ docs/run_demo.sh
@@ -1,12 +1,12 @@
-xelatex 青年2024.tex
-bibtex 青年2024
-xelatex 青年2024.tex
-xelatex 青年2024.tex
+# xelatex 青年正文2024.tex
+# bibtex 青年正文2024
+# xelatex 青年正文2024.tex
+# xelatex 青年正文2024.tex
 
-cp 青年2024.tex 青年2024-se.tex
-sed -i 's/\\usepackage[windows]{nsfc}/\\usepackage[windows]{nsfc_se}/' 青年2024-se.tex
-xelatex 青年2024-se.tex
-bibtex 青年2024-se
-xelatex 青年2024-se.tex
-xelatex 青年2024-se.tex
+cp 青年正文2024.tex 青年正文2024-se.tex
+sed -i 's/\\usepackage[windows]{nsfc}/\\usepackage[windows]{nsfc_se}/' 青年正文2024-se.tex
+# xelatex 青年正文2024-se.tex
+# bibtex 青年正文2024-se
+# xelatex 青年正文2024-se.tex
+# xelatex 青年正文2024-se.tex
 
